4e73e4fbbfcc59e626625025c9b913d6a1c0bea1
Rachit Ingole <79435749+Rachit-Ingole@users.noreply.github.com>
2025-01-21 18:57:33 +0530

    Made Auth Page response to Enter

--- frontend/src/components/Auth.jsx
+++ frontend/src/components/Auth.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 
 export default function Auth(props) {
   const {setToken,setData} = props
@@ -78,19 +78,13 @@ export default function Auth(props) {
 
   }
 
-  function handleSubmit(e){
-    if( !(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(email)){
-      return
-    }
-    console.log(email,username,password)
 
 
-  }
   
 
   return (
     page == "home" ? 
-    <div className='w-screen h-screen bg-slate-800 text-white '>
+    <div className='w-screen h-screen bg-slate-800 text-white ' >
       <div className='h-[90px] flex items-center justify-center w-full'>
       <h1 className='text-4xl font-Mont text-blue-300 font-md w-[85%]'>healthify</h1>
       </div>
@@ -110,7 +104,7 @@ export default function Auth(props) {
     </div>
     :
     (page == "login" ? 
-      <div className='w-screen h-screen bg-slate-800 text-white flex justify-center'>
+      <div className='w-screen h-screen bg-slate-800 text-white flex justify-center '  onKeyDown={(e)=>{if(e.key == "Enter"){handleLogin(e)}}}>
       <div className='auth-page flex w-screen justify-center align-center items-center text-white '>
         
         <div className='w-[400px] h-[500px] bg-slate-900 rounded-xl flex flex-col justify-center gap-[20px] border-t-4 border-blue-400 '>
@@ -123,11 +117,11 @@ export default function Auth(props) {
           <div className='flex flex-col justify-evenly h-1/2 items-center mb-[20px]'>
             <div className='mx-auto text-xl sm:text-2xl w-3/4'>
               <h1 className='font-Poppins text-xl'>Email:</h1>
-              <input value={email} onChange={(e)=>{setEmail(e.target.value)}} className='w-full pl-1 h-[35px] text-lg rounded-md text-slate-800 focus:outline-none'></input>
+              <input value={email} onChange={(e)=>{setEmail(e.target.value)}} className='w-full pl-1 h-[30px] text-lg rounded-md text-slate-800 focus:outline-none'></input>
             </div>
             <div className='mx-auto text-xl sm:text-2xl w-3/4'>
               <h1 className='font-Poppins text-xl'>Password:</h1>
-              <input value={password} onChange={(e)=>{setPassword(e.target.value)}} type='password' className='w-full pl-1 rounded-md text-slate-800'></input>
+              <input value={password} onChange={(e)=>{setPassword(e.target.value)}} type='password' className='w-full pl-1 h-[30px] text-lg rounded-md text-slate-800 focus:outline-none'></input>
             </div>
           </div>
           <div className='mx-auto alert box'>
@@ -143,7 +137,7 @@ export default function Auth(props) {
       </div>
     </div>     
       :
-    <div className='w-screen h-screen bg-slate-800 text-white flex justify-center'>
+    <div className='w-screen h-screen bg-slate-800 text-white flex justify-center' onKeyDown={(e)=>{if(e.key == "Enter"){handleRegister(e)}}}>
       <div className='auth-page flex w-screen justify-center align-center items-center text-white '>
         
         <div className='w-[400px] h-[500px] bg-slate-900 rounded-xl flex flex-col justify-center gap-[20px] border-t-4 border-blue-400 '>
